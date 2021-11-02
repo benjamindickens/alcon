e8e535b4ab3e124b958e3b58534f44ee2ae260ff
Boris Mitrofanov <boryamitrofanov@gmail.com>
2021-11-02 10:46:21 +0300

    form done

--- src/js/block-3.js
+++ src/js/block-3.js
@@ -12,7 +12,8 @@ const app = new Vue({
             authMethod: "phone",
             gettingPhoneCode: false,
             countDown: 0,
-            authNewUser: false,
+            registerNewUser: false,
+            registeredUser: false,
             formData: {
                 phone: "",
                 phoneCode: "",
@@ -20,6 +21,7 @@ const app = new Vue({
                 password: "",
                 rePassword: "",
                 agreement: "",
+                name: "",
             },
             errors: {
                 phoneCode: "",
@@ -28,6 +30,7 @@ const app = new Vue({
                 password: "",
                 rePassword: "",
                 agreement: "",
+                name: "",
             },
             validation: {
                 phone: "",
@@ -36,6 +39,7 @@ const app = new Vue({
                 password: "",
                 rePassword: "",
                 agreement: "",
+                name: "",
             }
         }
     }, watch: {
@@ -55,27 +59,38 @@ const app = new Vue({
             }
         },
         'formData.password'(val) {
-            if (val) {
-                this.validation.password = true;
-            } else {
-                this.validation.password = false;
-            }
+            this.checkIsEmpty(val, "password")
+        },
+        'formData.rePassword'(val) {
+            this.checkIsEmpty(val, "rePassword")
+        },
+        'formData.name'(val) {
+            this.checkIsEmpty(val, "name")
+        },
+        'formData.agreement'(val) {
+            this.checkIsEmpty(val, "agreement")
         },
         'formData.phoneCode'(val) {
             if (val.length > 4) {
                 this.formData.phoneCode = val.splice(0, 3);
             }
             if (val.length === 4) {
+                //reuest to check pincode
                 const rightPin = "0000";
+                let alreadyExist = false;
                 if (val !== rightPin) {
                     this.formData.phoneCode = '';
                     this.errors.phoneCode = "Неправильно введен код";
                 } else {
-                    this.authNewUser = true;
+                    if (alreadyExist) {
+                        this.registeredUser = true;
+                    } else {
+                        this.registerNewUser = true;
+                    }
+
                 }
                 document.activeElement.blur();
             }
-
         }
     },
     computed: {
@@ -84,6 +99,35 @@ const app = new Vue({
         }
     },
     methods: {
+        checkIsEmpty(val, field) {
+            if (val) {
+                this.validation[field] = true;
+            } else {
+                this.validation[field] = false;
+            }
+        },
+        registration() {
+            let validData = true;
+            if (this.formData.password != this.formData.rePassword) {
+                validData = false;
+                this.formData.password = ""
+                this.formData.rePassword = ""
+                this.errors.rePassword = "Пароли не совпадают";
+                this.errors.password = "Пароли не совпадают";
+            }
+            for (const [field, value] of Object.entries(this.formData)) {
+                if (value === "") {
+                    validData = false;
+                    this.errors[field] = "Поле обязательно для заполнения"
+                }
+            }
+
+            if (validData) {
+                //    send data to register
+                console.log("registered")
+                this.registeredUser = true;
+            }
+        },
         validateEmail(mail) {
             if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(mail)) {
                 return (true)
